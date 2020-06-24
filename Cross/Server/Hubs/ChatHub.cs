ba37b95b3d93259d84d87a3df53350514e0e2db2
Fred Bainbridge <Fred@teambainbridge.com>
2020-06-24 05:55:40 -0500

    fixing gamehub

--- Cross/Server/Hubs/ChatHub.cs
+++ Cross/Server/Hubs/ChatHub.cs
@@ -12,5 +12,15 @@ namespace Cross.Server.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+        public async Task DealMessage()
+        {
+            var cards = new List<int>();
+            var rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                cards.Add(rand.Next());
+            }
+            await Clients.All.SendAsync("ReceiveBoard", cards);
+        }
     }
 }
--- Cross/Server/Hubs/DealHub.cs
+++ Cross/Server/Hubs/DealHub.cs
@@ -8,15 +8,6 @@ namespace Cross.Server.Hubs
 {
     public class DealHub : Hub
     {
-        public async Task DealMessage()
-        {
-            var cards = new List<int>();
-            var rand = new Random();
-            for (int i = 0; i < 5; i++)
-            {
-                cards.Add(rand.Next());
-            }
-            await Clients.All.SendAsync("ReceiveBoard", cards);
-        }
+        
     }
 }
